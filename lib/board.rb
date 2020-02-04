e43978e6116fbd7b4185745b1b2472d7097b83c5
Javier Aguilar <jvr.agx@gmail.com>
2020-02-04 15:27:41 -0700

    Add generate board method and valid_coordinate? method

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,32 @@
 class Board
+  attr_reader :cells
 
   def initialize
+    @cells = {}
   end
 
+  def generate(size)
+    #letters = ('a'..'z').to_a
+    count = 1
+    size.times do
+      if count <= 4
+        key = "A#{count}"
+        @cells[key] = Cell.new(key)
+      elsif count <= 8
+        key = "B#{count-4}"
+        @cells[key] = Cell.new(key)
+      elsif count <= 12
+        key = "C#{count-8}"
+        @cells[key] = Cell.new(key)
+      elsif count <= 16
+        key = "D#{count-12}"
+        @cells[key] = Cell.new(key)
+      end
+      count += 1
+    end
+  end
+
+  def valid_coordinate?(coordinate)
+    @cells.key? coordinate
+  end
 end
