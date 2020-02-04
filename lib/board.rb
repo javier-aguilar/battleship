f454d8a9df953591aaabc12145e81844876cf26e
Javier Aguilar <jvr.agx@gmail.com>
2020-02-04 16:32:51 -0700

    Clean up comments

--- lib/board.rb
+++ lib/board.rb
@@ -6,7 +6,7 @@ class Board
   end
 
   def generate(size)
-    #letters = ('a'..'z').to_a
+    #code based on 4 x 4 currently
     count = 1
     size.times do
       if count <= 4
@@ -32,7 +32,7 @@ class Board
 
   def valid_placement?(ship, coordinates)
     if ship.length == coordinates.length
-      if is_vertical?(coordinates) || is_horizontal?(coordinates)
+      if can_place_vertical?(coordinates) || can_place_horizontal?(coordinates)
         true
       else
         false
@@ -42,23 +42,18 @@ class Board
     end
   end
 
-  def is_vertical?(coordinates)
+  def can_place_vertical?(coordinates)
     range = []
     @cells.map {|cell_key, cell_value| range << cell_key}
-    #checks vertically
     coordinates.each_cons(2).all? do | coordinate1, coordinate2 |
       coordinate1[0] == coordinate2[0] && coordinate1[1].to_i == (coordinate2[1].to_i - 1)
     end
   end
 
-  def is_horizontal?(coordinates)
-    # A = 65
-    # B = 66
-    # C = 67
-    # D = 68
+  def can_place_horizontal?(coordinates)
+    # A = 65, B = 66, C = 67, D = 68
     range = []
     @cells.map {|cell_key, cell_value| range << cell_key}
-    #checks vertically
     coordinates.each_cons(2).all? do | coordinate1, coordinate2 |
       coordinate1[1] == coordinate2[1] && coordinate1[0].ord == (coordinate2[0].ord - 1)
     end
