8ff1636fec749b61aba8a35082629a20e7a6d0d6
Javier Aguilar <jvr.agx@gmail.com>
2020-02-04 15:28:03 -0700

    Pass test_if_it_is_valid_coordinate?

--- test/board_test.rb
+++ test/board_test.rb
@@ -12,4 +12,23 @@ class BoardTest < Minitest::Test
     assert_instance_of Board, board
   end
 
+
+  def test_it_has_cells
+    board = Board.new
+    cells = {}
+
+    assert_equal cells, board.cells
+  end
+
+  def test_if_it_is_valid_coordinate?
+    board = Board.new
+    board.generate(16)
+
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal true, board.valid_coordinate?("D4")
+    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal false, board.valid_coordinate?("E1")
+    assert_equal false, board.valid_coordinate?("A22")
+  end
+
 end
