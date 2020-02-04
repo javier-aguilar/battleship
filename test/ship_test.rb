fae68e1faac7f4b079827576883dd665f39b37a8
Javier Aguilar <jvr.agx@gmail.com>
2020-02-04 12:48:19 -0700

    Pass test_it_has_a_length

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -6,13 +6,16 @@ class ShipTest < Minitest::Test
 
   def test_it_exists
     cruiser = Ship.new("Cruiser", 3)
-
     assert_instance_of Ship, cruiser
   end
 
   def test_it_has_a_name
     cruiser = Ship.new("Cruiser", 3)
-
     assert_equal "Cruiser", cruiser.name
+  end
+
+  def test_it_has_a_length
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal 3, cruiser.length
   end
 end
