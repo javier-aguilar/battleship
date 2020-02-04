8f0ddd1a66200472367d2c4bd6576be1d4e684e6
Javier Aguilar <jvr.agx@gmail.com>
2020-02-04 16:10:18 -0700

    Add checks for valid placements

--- lib/board.rb
+++ lib/board.rb
@@ -29,4 +29,38 @@ class Board
   def valid_coordinate?(coordinate)
     @cells.key? coordinate
   end
+
+  def valid_placement?(ship, coordinates)
+    if ship.length == coordinates.length
+      if is_vertical?(coordinates) || is_horizontal?(coordinates)
+        true
+      else
+        false
+      end
+    else
+      false
+    end
+  end
+
+  def is_vertical?(coordinates)
+    range = []
+    @cells.map {|cell_key, cell_value| range << cell_key}
+    #checks vertically
+    coordinates.each_cons(2).all? do | coordinate1, coordinate2 |
+      coordinate1[0] == coordinate2[0] && coordinate1[1].to_i == (coordinate2[1].to_i - 1)
+    end
+  end
+
+  def is_horizontal?(coordinates)
+    # A = 65
+    # B = 66
+    # C = 67
+    # D = 68
+    range = []
+    @cells.map {|cell_key, cell_value| range << cell_key}
+    #checks vertically
+    coordinates.each_cons(2).all? do | coordinate1, coordinate2 |
+      coordinate1[1] == coordinate2[1] && coordinate1[0].ord == (coordinate2[0].ord - 1)
+    end
+  end
 end
