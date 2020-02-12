8d0230dbfbd2690058536c5b248024d463095260
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-11 20:49:29 -0700

    Add test_it_returns_results

--- test/game_test.rb
+++ test/game_test.rb
@@ -16,13 +16,13 @@ class GameTest < Minitest::Test
     @game.user_info[:board].generate
 
     @game.computer_info[:ships] = {
-      submarine: Ship.new('Submarine', 2),
-      cruiser: Ship.new('Cruiser', 3)
+      submarine: Ship.new('Submarine', 3),
+      cruiser: Ship.new('Cruiser', 2)
     }
 
     @game.user_info[:ships] = {
-      submarine: Ship.new('Submarine', 2),
-      cruiser: Ship.new('Cruiser', 3)
+      submarine: Ship.new('Submarine', 3),
+      cruiser: Ship.new('Cruiser', 2)
     }
   end
 
@@ -32,11 +32,8 @@ class GameTest < Minitest::Test
   end
 
   def test_summary_if_player_won
-    @game.computer_info[:board].place(@game.computer_info[:ships][:submarine], ["A1", "A2", "A3"])
-    @game.computer_info[:board].place(@game.computer_info[:ships][:cruiser], ["B1", "B2"])
-
-    3.times { @game.computer_info[:ships][:cruiser].hit }
-    2.times { @game.computer_info[:ships][:submarine].hit }
+    2.times { @game.computer_info[:ships][:cruiser].hit }
+    3.times { @game.computer_info[:ships][:submarine].hit }
 
     expected = "\n*******************\nYou won!\n*******************".green
     assert_equal expected, @game.summary
@@ -46,21 +43,57 @@ class GameTest < Minitest::Test
     @game.user_info[:board].place(@game.user_info[:ships][:submarine], ["A1", "A2", "A3"])
     @game.user_info[:board].place(@game.user_info[:ships][:cruiser], ["B1", "B2"])
 
-    3.times { @game.user_info[:ships][:cruiser].hit }
-    2.times { @game.user_info[:ships][:submarine].hit }
+    2.times { @game.user_info[:ships][:cruiser].hit }
+    3.times { @game.user_info[:ships][:submarine].hit }
 
     expected = "\n*******************\nI won! ( ⓛ ω ⓛ *)\n*******************".red
     assert_equal expected, @game.summary
   end
 
-  def test_game_over
+  def test_is_game_over
     @game.user_info[:board].place(@game.user_info[:ships][:submarine], ["A1", "A2", "A3"])
     @game.user_info[:board].place(@game.user_info[:ships][:cruiser], ["B1", "B2"])
     assert_equal false, @game.is_game_over?
 
-    3.times { @game.user_info[:ships][:cruiser].hit }
-    2.times { @game.user_info[:ships][:submarine].hit }
+    2.times { @game.user_info[:ships][:cruiser].hit }
+    3.times { @game.user_info[:ships][:submarine].hit }
     assert_equal true, @game.is_game_over?
   end
 
+  def test_it_returns_results
+
+    @game.computer_info[:board].place(@game.computer_info[:ships][:submarine], ["A1", "A2", "A3"])
+    @game.computer_info[:board].place(@game.computer_info[:ships][:cruiser], ["B1", "B2"])
+
+    @game.user_info[:board].place(@game.user_info[:ships][:submarine], ["A1", "A2", "A3"])
+    @game.user_info[:board].place(@game.user_info[:ships][:cruiser], ["B1", "B2"])
+
+    @game.computer_info[:board].cells["C1"].fire_upon
+    expected = "Your shot on C1 was a miss.".red
+    assert_equal expected, @game.results(@game.computer_info[:board], "C1")
+
+    @game.user_info[:board].cells["C1"].fire_upon
+    expected = "My shot on C1 was a miss."
+    assert_equal expected, @game.results(@game.user_info[:board], "C1", "computer")
+
+    @game.computer_info[:board].cells["A1"].fire_upon
+    expected = "Your shot on A1 was a hit.".red
+    assert_equal expected, @game.results(@game.computer_info[:board], "A1")
+
+    @game.user_info[:board].cells["A1"].fire_upon
+    expected = "My shot on A1 was a hit."
+    assert_equal expected, @game.results(@game.user_info[:board], "A1", "computer")
+
+    @game.computer_info[:board].cells["A2"].fire_upon
+    @game.computer_info[:board].cells["A3"].fire_upon
+    expected = "Your shot on A3 sunk my ship.".red
+    assert_equal expected, @game.results(@game.computer_info[:board], "A3")
+
+    @game.user_info[:board].cells["A2"].fire_upon
+    @game.user_info[:board].cells["A3"].fire_upon
+    expected = "My shot on A3 sunk your ship."
+    assert_equal expected, @game.results(@game.user_info[:board], "A3", "computer")
+
+
+  end
 end
